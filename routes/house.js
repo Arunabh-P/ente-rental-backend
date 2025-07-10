2824eddb343a47549660d127ac9a497de6d732df
Arunabh <96439469+Arunabh-P@users.noreply.github.com>
2025-07-10 16:33:25 +0530

    get houseBySlug 02

--- routes/house.js
+++ routes/house.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { createHouse, getHouseByID, getHouses, updateHouseById } from '../controller/house.js';
+import { createHouse, getHouseByID, getHouseBySlug, getHouses, updateHouseById } from '../controller/house.js';
 
 const router = express.Router();
 
 router.post('/create', createHouse)
 router.get('/', getHouses); 
 router.get("/:id", getHouseByID)
+router.get("/single/:slug", getHouseBySlug)
 router.put('/:id',updateHouseById)
 
 export default router
