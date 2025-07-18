22babdb85d6f2412b7581ac4c37b242951b9d486
Arunabh <96439469+Arunabh-P@users.noreply.github.com>
2025-07-18 11:52:49 +0530

    changes for next auth

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { authUserDetails, loginUser, logoutUser, refreshUserToken, registerUser } from "../controller/user.js";
+import { getUserProfile, loginUser, registerUser, updateUser, veryfyNextAuthToken } from "../controller/user";
 export const router = express.Router();
 
 router.post("/register",  registerUser);
 router.post("/login", loginUser);
-router.post("/refresh-token", refreshUserToken);
-router.get("/auth-details", authUserDetails);
-router.post("/logout", logoutUser);
+router.get("/profile", veryfyNextAuthToken,getUserProfile);
+router.put("/profile", veryfyNextAuthToken,updateUser);
+
 
 export default router;
